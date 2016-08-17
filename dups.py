a77f2e3e6068d13cffa8e40d27e4ad09bf4ab468
Robert Hohman <rhohman@rhohman.odonnellpdc.com>
2016-08-17 17:17:17 -0400

    update dups.py to be pep8 compatible

--- dups.py
+++ dups.py
@@ -1,7 +1,7 @@
 import csv
 
 
-CSV_FILES = ["test.csv","test_2.csv","test_3.csv"]
+CSV_FILES = ["test.csv", "test_2.csv", "test_3.csv"]
 TEST_FIELD = "field_1"
 ITEMS = dict()
 
@@ -10,18 +10,20 @@ for csv_file in CSV_FILES:
         with open(csv_file, 'rb') as csvfile:
                 csv_dict = csv.DictReader(csvfile)
                 for row in csv_dict:
-                        if row[TEST_FIELD] not in ITEMS.keys() :
+                        if row[TEST_FIELD] not in ITEMS.keys():
                                 ITEMS[row[TEST_FIELD]] = [csv_file]
                         else:
                                 ITEMS[row[TEST_FIELD]].append(csv_file)
-                                
-                                
-out_str = "For the field '{}' there were duplicates in the following:".format(TEST_FIELD)
+
+
+out_str = """
+For the field '{}' there were duplicates in the following:
+""".format(TEST_FIELD)
+
 for item in ITEMS.keys():
         if len(ITEMS[item]) > 1:
-                out_str += "For item '{}', check in the files '{}' ".format(item, ITEMS[item])
+                out_str += """
+                For item '{}', check in the files '{}'
+                """.format(item, ITEMS[item])
 
 print out_str
-
-
-
