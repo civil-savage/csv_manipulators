8d2553e37a4af14472dda9c5e97fafee162ca9fb
Robert Hohman <rhohman@rhohman.odonnellpdc.com>
2016-08-24 16:58:12 -0400

    added argparse to dups

--- dups.py
+++ dups.py
@@ -1,24 +1,31 @@
+import argparse
 import csv
 
 
-CSV_FILES = ["test.csv", "test_2.csv", "test_3.csv"]
-TEST_FIELD = "field_1"
+parser = argparse.ArgumentParser("Checks a lsit of csv files for duplicates.")
+
+parser.add_argument('field', nargs=1, type=str,
+                    help="A field to check for duplicates.")
+parser.add_argument('csv_files', nargs='*',
+                    help="A list of csv files to check.")
+
+
+args = parser.parse_args()
 ITEMS = dict()
+FIELD = args.field.pop()
 
-
-for csv_file in CSV_FILES:
+for csv_file in args.csv_files:
         with open(csv_file, 'rb') as csvfile:
                 csv_dict = csv.DictReader(csvfile)
                 for row in csv_dict:
-                        if row[TEST_FIELD] not in ITEMS.keys():
-                                ITEMS[row[TEST_FIELD]] = [csv_file]
+                        if row[FIELD] not in ITEMS.keys():
+                                ITEMS[row[FIELD]] = [csv_file]
                         else:
-                                ITEMS[row[TEST_FIELD]].append(csv_file)
-
+                                ITEMS[row[FIELD]].append(csv_file)
 
 out_str = """
 For the field '{}' there were duplicates in the following:
-""".format(TEST_FIELD)
+""".format(FIELD)
 
 for item in ITEMS.keys():
         if len(ITEMS[item]) > 1:
